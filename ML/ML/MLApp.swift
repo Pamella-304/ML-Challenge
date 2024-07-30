b3f7cdfc356a266352cb043d6985996fab90984e
Pamella <pamella.as.304@gmail.com>
2024-07-30 17:40:40 -0300

    created the xCode file

---
+++ ML/ML/MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLApp.swift
+//  ML
+//
+//  Created by Pamella Alvarenga on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
